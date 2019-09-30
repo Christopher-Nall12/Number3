fcebae7f61582d2ca5067c42f12b09b7859c2008
Christopher-Nall <bigcmn@gmail.com>
2019-09-30 15:37:47 -0500

    Playing with words

--- 1st #3.py
+++ 1st #3.py
@@ -1,3 +1,6 @@
 print("hello world")
 print("howdy y'all")
 print("how have yall been doin. Im pretty good, how bout yall")
+print("i hate writing essays and anything that is going to be graded by my teacher reading it")
+
+print(55 + 7094)
