8781f2bbc26750ffd9c093c1be7346fd6db9ea90
Christopher-Nall <bigcmn@gmail.com>
2019-10-01 15:18:26 -0500

    Playing with words

--- 1st #3.py
+++ 1st #3.py
@@ -1,6 +1,13 @@
-print("hello world")
-print("howdy y'all")
-print("how have yall been doin. Im pretty good, how bout yall")
-print("i hate writing essays and anything that is going to be graded by my teacher reading it")
+# this is how you take notes on python
+# <--- this is the  thing you use. The octothorpe
 
-print(55 + 7094)
+# using the word PRINT, you can tell the computer to write what ever you put in the parentheses and quotation marks
+
+print("This is what it looks like when you use print")
+
+
+
+
+
+
+print(55 * 7094 + 5 * 35)
