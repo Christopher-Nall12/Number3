edbd0c795ddfc0ff837d3eb51fbf42555a5e337d
Christopher-Nall <bigcmn@gmail.com>
2019-10-03 15:55:07 -0500

    Adding variables

---
+++ Variables.py
@@ -0,0 +1,21 @@
+# This is my new variable story
+
+myFace = "is very ugly"
+myMax = "45lbs on bench press"
+myGPA = 5.25
+myRelationship = "single and lonely"
+myParents = "they think im useless in any circumstance"
+myAverage = 110.535208
+myTool = "mental math"
+
+# s is for words
+# d is for numbers
+# f adds decimal points, maybe a little to much but it works
+# e adds infinite decimal points/ shows all place holdings
+
+print("i am %s" % myRelationship)
+print("my parents hate me and %s" % myParents)
+print("im not strong and i only have a max of %s" % myMax)
+print("everyone makes fun of me because my face %s" % myFace)
+print("but actually im very smart and have a %f GPA" % myGPA)
+print("i am the smartest in every class and never make below %e \n which i obviously did that calculation in my head with my greatest tool, %s" % (myAverage, myTool))
